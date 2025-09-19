fdd36e24a7b2685ec5415345628f30ca53a7bcb8
HeiTasca <123heitor.hh@gmail.com>
2025-09-19 11:57:00 -0300

    DanteV3
    Agora ta certo

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,81 @@
 #include <Arduino.h>
+#include <HCSR04.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define p_trig 5
+#define p_echo 3
+#define buz 8
+#define botao 7
+
+UltraSonicDistanceSensor distanceSensor(p_trig, p_echo);
+const int In3 = 9;
+const int In4 = 10;
+const int In1 = 6;
+const int In2 = 11;
+
+bool sistemaLigado = false; 
+bool ultimoEstadoBotao = HIGH;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(In3, OUTPUT);
+  pinMode(In4, OUTPUT);
+  pinMode(In1, OUTPUT);
+  pinMode(In2, OUTPUT);
+  pinMode(buz, OUTPUT);
+  pinMode(botao, INPUT_PULLUP);
+
+  digitalWrite(In3, LOW);
+  digitalWrite(In4, LOW);
+  digitalWrite(In1, LOW);
+  digitalWrite(In2, LOW);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  bool estadoBotao = digitalRead(botao);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (estadoBotao == LOW && ultimoEstadoBotao == HIGH) {
+    sistemaLigado = !sistemaLigado;
+    delay(200);
+  }
+  ultimoEstadoBotao = estadoBotao;
+
+  if (sistemaLigado) {
+    float distance = distanceSensor.measureDistanceCm();
+
+    if (distance != -1) {
+      Serial.println(distance);
+    } else {
+      Serial.println("Erro: Sensor de distancia nao conseguiu uma leitura valida.");
+    }
+
+    if (distance > 0 && distance <= 30) {
+      tone(buz, 320);
+      digitalWrite(In3, LOW);
+      digitalWrite(In4, LOW);
+      digitalWrite(In1, HIGH);
+      digitalWrite(In2, LOW);
+    } else if (distance > 30 && distance <= 40) {
+      tone(buz, 320);
+      delay(100);
+      tone(buz, 0);
+      delay(100);
+      digitalWrite(In3, HIGH);
+      digitalWrite(In4, LOW);
+      digitalWrite(In1, LOW);
+      digitalWrite(In2, HIGH);
+    } else if (distance > 20 && distance <= 100) {
+      noTone(buz);
+      digitalWrite(In3, HIGH);
+      digitalWrite(In4, LOW);
+      digitalWrite(In1, LOW);
+      digitalWrite(In2, HIGH);
+    }
+  } else {
+    noTone(buz);
+    digitalWrite(In3, LOW);
+    digitalWrite(In4, LOW);
+    digitalWrite(In1, LOW);
+    digitalWrite(In2, LOW);
+  }
 }
