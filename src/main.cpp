e6471494d6c613b40906b463f127fad9f071ebf8
HeiTasca <123heitor.hh@gmail.com>
2025-09-20 16:21:32 -0300

    DanteV4_Leds
    Agora tem leds funcionais para indicar se o dante esta pertou ou não de obstaculos.

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,16 @@
 #define p_echo 3
 #define buz 8
 #define botao 7
+#define ENA 2
 
 UltraSonicDistanceSensor distanceSensor(p_trig, p_echo);
 const int In3 = 9;
 const int In4 = 10;
 const int In1 = 6;
 const int In2 = 11;
+const int Vermelho = 4;
+const int Verde = 12;
+
 
 bool sistemaLigado = false; 
 bool ultimoEstadoBotao = HIGH;
@@ -21,7 +25,10 @@ void setup() {
   pinMode(In1, OUTPUT);
   pinMode(In2, OUTPUT);
   pinMode(buz, OUTPUT);
+  pinMode(Vermelho, OUTPUT);
+  pinMode(Verde, OUTPUT);
   pinMode(botao, INPUT_PULLUP);
+  pinMode(ENA, OUTPUT);
 
   digitalWrite(In3, LOW);
   digitalWrite(In4, LOW);
@@ -52,24 +59,22 @@ void loop() {
     if (distance > 0 && distance <= 30) {
       tone(buz, 320);
       digitalWrite(In3, LOW);
-      digitalWrite(In4, LOW);
-      digitalWrite(In1, HIGH);
-      digitalWrite(In2, LOW);
-    } else if (distance > 30 && distance <= 40) {
-      tone(buz, 320);
-      delay(100);
-      tone(buz, 0);
-      delay(100);
-      digitalWrite(In3, HIGH);
-      digitalWrite(In4, LOW);
+      digitalWrite(In4, HIGH);
+      analogWrite(ENA, 100);
       digitalWrite(In1, LOW);
       digitalWrite(In2, HIGH);
+      digitalWrite(Vermelho, HIGH);
+      digitalWrite(Verde, LOW);
+    } else if (distance > 30 && distance <= 40) {
+
     } else if (distance > 20 && distance <= 100) {
       noTone(buz);
       digitalWrite(In3, HIGH);
       digitalWrite(In4, LOW);
       digitalWrite(In1, LOW);
       digitalWrite(In2, HIGH);
+      digitalWrite(Vermelho, LOW);
+      digitalWrite(Verde, HIGH);
     }
   } else {
     noTone(buz);
